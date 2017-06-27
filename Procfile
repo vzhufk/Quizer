bf388f83737e60dd5d9a157c4ddd1186b830632b
zhufyakvv <zhufyakvv@gmail.com>
2017-06-27 21:59:09 +0300

    try

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn bizicoTest.wsgi --log-file -
--- bizicoTest/settings.py
+++ bizicoTest/settings.py
@@ -123,3 +123,21 @@ AUTH_USER_MODEL = 'quiz.User'
 # Media
 MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
+
+# HEROKU
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
